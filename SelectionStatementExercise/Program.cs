3b3382bd9eeee8cec8340e4e756a42272660b201
Keyan117 <kdaneshvari98@gmail.com>
2023-03-31 12:07:40 -0700

    sse

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,53 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+          //Number Guessing Game
+          var r = new Random();
+          var favNumber = r.Next(1, 1001);
+          
+
+          Console.WriteLine("Pick as favNumber from 1 - 1000");
+          var userInput = int.Parse(Console.ReadLine());
+
+          if(userInput < favNumber)
+          {
+            Console.WriteLine($"{userInput} is too low");
+          }
+          else if(userInput > favNumber)
+          {
+            Console.Writeline ($"{userInput} is too high");
+          }
+          else
+          {
+            Console.Writline("You guessed it!");
+          }
+
+          //Switch-Case
+          Console.WriteLine("What is your favorite subject?");
+        
+          string subject = Console.ReadLine();
+
+          switch (subject.ToLower())
+          {
+            case "math":
+                Console.WriteLine["I hate Math!"];
+                break;
+            case "science":
+                Console.WriteLine["I love science!"];
+                break;
+            case "pe":
+                Console.WriteLine["PE is the best!!"];
+                break;
+            case "history":
+                Console.WriteLine["History is fun."];
+                break;
+            case "english":
+                Console.WriteLine["English is boring."];
+                break;
+            default:
+                Console.WriteLine["I don't know that subject."];
+                break;
+           }
         }
     }
 }
